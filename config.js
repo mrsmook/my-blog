86c02bde65441cc19e63dbf833b41d8fb9dd5cbf
zaouali.mohamed.selim@gmail.com <zaouali.mohamed.selim@gmail.com>
2019-04-04 22:50:43 +0200

    personal changes

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
+  title: 'my Blog', // Required
+  author: 'Selim Zaouali', // Required
   description: 'Full-stack Web Developer',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://selim-zaouali.netlify.com',
+    github: 'https://github.com/mrsmook',
+    twitter: 'https://twitter.com/mrsmook',
+    linkedin: 'https://ca.linkedin.com/in/selim-zaouali',
   },
-  pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  pathPrefix: '/',
+  siteUrl: 'https://mrsmook-blog.netlify.com/',
 };
